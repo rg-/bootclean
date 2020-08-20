2fec317735d0bbd54dd222212457f34a0a1e0249
rg- <roberto@rgdesign.org>
2020-08-20 19:36:42 -0300

    Update woocommerce.php

--- bc/core/woocommerce.php
+++ bc/core/woocommerce.php
@@ -16,13 +16,16 @@ if ( WPBC_is_woocommerce_active() ) {
 		add_action( 'after_setup_theme', function() {
 			add_theme_support( 'woocommerce' );
 		} );
+
 		// $path defaults to 'woocommerce/' (in your theme folder)
+		/*
 		add_filter( 'woocommerce_template_path', function( $path ){
 				$custom_path_parent = get_template_directory() . '/template-parts/woocommerce/';
 		    $custom_path_child = get_stylesheet_directory() . '/template-parts/woocommerce/';
 		    return ( file_exists( $custom_path_parent ) || file_exists( $custom_path_child ) ) ? 'template-parts/woocommerce/' : $path;
 		}, 10);
-
+		*/
+		
 		// Customizations disabled by default just in case need other customization
 		$WPBC_woocommerce_customize = apply_filters('wpbc/filter/woocommerce/enable_customise',false);
 		if(!empty($WPBC_woocommerce_customize)){
