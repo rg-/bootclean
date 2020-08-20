960e236cee9a5d3e9f6fea158ed3f2b3ef05ef54
rg- <roberto@rgdesign.org>
2020-08-19 22:38:13 -0300

    Update bc-codemirror.php

--- bc/core/addons/bc-codemirror.php
+++ bc/core/addons/bc-codemirror.php
@@ -425,7 +425,8 @@ add_action( 'init', 'WPBC_codemirror_cookies' );
 
 function WPBC_codemirror_cookies() {
 	if( !isset($_COOKIE['wpbc_codemirror_size']) ) {
-	 	setcookie( 'wpbc_codemirror_size', 'md', time() + 3600 * 24 * 100, COOKIEPATH, COOKIE_DOMAIN, false );
+		setcookie( $rp_cookie, ' ', time() + 3600 * 24 * 100, COOKIEPATH, COOKIE_DOMAIN, is_ssl(), true );
+	 //	setcookie( 'wpbc_codemirror_size', 'md', time() + 3600 * 24 * 100, COOKIEPATH, COOKIE_DOMAIN, false );
 	 }
 }
 
